e0ad85fba004e90054591467e438652e374ab53f
wojtus <wojtus@spectrum>
2018-08-02 19:17:42 +0200

    added readiness & livenessProbe

--- src/main/java/spectum/openshiftdemo/Config.java
+++ src/main/java/spectum/openshiftdemo/Config.java
@@ -18,13 +18,25 @@ public class Config {
 
 	@Bean
 	public RouterFunction<ServerResponse> route() {
-		RequestPredicate reqestPredicate = RequestPredicates.GET("/demo").//
+		RequestPredicate reqestPredicate = RequestPredicates.GET("/").//
 				and(RequestPredicates.accept(MediaType.TEXT_PLAIN));
-		return RouterFunctions.route(reqestPredicate, this::hello);
+		RequestPredicate aliveReqestPredicate = RequestPredicates.GET("/alive");
+		RequestPredicate readyReqestPredicate = RequestPredicates.GET("/ready");
+		return RouterFunctions.route(reqestPredicate, this::hello).//
+				and(RouterFunctions.route(aliveReqestPredicate, this::alive))
+				.and(RouterFunctions.route(readyReqestPredicate, this::ready));
 	}
 
-	public Mono<ServerResponse> hello(ServerRequest request) {
+	private Mono<ServerResponse> hello(ServerRequest request) {
 		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(BodyInserters.fromObject("Yep\n"));
+	}
+
+	private Mono<ServerResponse> ready(ServerRequest request) {
+		return ServerResponse.ok().build();
+	}
+
+	private Mono<ServerResponse> alive(ServerRequest request) {
+		return ServerResponse.ok().build();
 	}
 
 }
