575b7caee5c4fc80325ebf6e4d923b2adbb65698
wojtus <wojtus@spectrum>
2018-08-03 23:37:14 +0200

    Added controller

---
+++ src/main/java/spectum/openshiftdemo/person/PersonController.java
@@ -0,0 +1,25 @@
+package spectum.openshiftdemo.person;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/person")
+class PersonController {
+
+	private final PersonService personService;
+
+	PersonController(PersonService personService) {
+		this.personService = personService;
+	}
+
+	@GetMapping("/all")
+	Flux<Person> getAllPersons() {
+		return personService.getPersons();
+
+	}
+
+}
---
+++ src/test/java/spectum/openshiftdemo/person/PersonControllerTest.java
@@ -0,0 +1,29 @@
+package spectum.openshiftdemo.person;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest(PersonController.class)
+public class PersonControllerTest {
+
+	@Autowired
+	private WebTestClient webClient;
+	@MockBean
+	private PersonRepository personRepository;
+	@MockBean
+	private PersonService personService;
+
+	@Test
+	public void testGetAllPersons() throws Exception {
+		webClient.get().uri("/person/all").accept(MediaType.APPLICATION_JSON).//
+				exchange().expectStatus().isOk();
+	}
+
+}
