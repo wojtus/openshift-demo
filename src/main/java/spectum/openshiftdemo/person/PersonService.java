f9f33b8dc97ac3285a5ae5f613e8fdb4f9732cf1
wojtus <wojtus@spectrum>
2018-08-04 15:05:01 +0200

    Added some logic

--- src/main/java/spectum/openshiftdemo/person/PersonService.java
+++ src/main/java/spectum/openshiftdemo/person/PersonService.java
@@ -1,5 +1,14 @@
 package spectum.openshiftdemo.person;
 
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.reactivestreams.Publisher;
@@ -10,10 +19,24 @@ import reactor.core.publisher.Flux;
 @Service
 class PersonService {
 
+	private final static List<String> CITIES = Arrays.asList("Berlin", "Hamburg", "München", "Köln",
+			"Frankfurt am Main", "Stuttgart", "Düsseldorf", "Dortmund", "Essen", "Leipzig");
+	private final static List<String> FORENAMES = Arrays.asList("Mia", "Ben", "Emma", "Jonas", "Hannah", "Leon",
+			"Sophia", "Elias", "Anna", "Fynn");
+	private final static List<String> SURNAMES = Arrays.asList("Müller", "Schmidt", "Schneider", "Fischer", "Weber",
+			"Meyer", "Wagner", "Becker", "Schulz", "Hoffmann");
 	private final PersonRepository personRepository;
 
+	private final Clock clock;
+
 	PersonService(PersonRepository personRepository) {
+		this(personRepository, Clock.systemDefaultZone());
+
+	}
+
+	PersonService(PersonRepository personRepository, Clock clock) {
 		this.personRepository = personRepository;
+		this.clock = clock;
 	}
 
 	Flux<Person> getPersons() {
@@ -27,9 +50,57 @@ class PersonService {
 	}
 
 	private Person createRandomPerson() {
+		String forename = getRandomForename();
+		String surname = getRandomSurname();
+		LocalDate birthDate = getRandomBirthDate();
+		List<String> citiesOfLiving = getRandomCitiesOfLiving(birthDate);
+
 		Person person = new Person();
+		person.setForename(forename);
+		person.setSurname(surname);
+		person.setCitiesOfLiving(citiesOfLiving);
+		person.setBirthDate(birthDate);
 		return person;
 
+	}
+
+	List<String> getRandomCitiesOfLiving(LocalDate birthDate) {
+		long yearsOfLiving = ChronoUnit.YEARS.between(birthDate, LocalDate.now(clock));
+		double averageYearsOfLivingInACity = Math.abs(ThreadLocalRandom.current().nextGaussian() * 4 + 16);
+		int numberOfCities = (int) (yearsOfLiving / averageYearsOfLivingInACity) + 1;
+		List<String> citiesOfLiving = Stream.generate(() -> getRandomValue(CITIES)).limit(numberOfCities)
+				.collect(Collectors.toList());
+		return citiesOfLiving;
+	}
+
+	LocalDate getRandomBirthDate() {
+		Random random = ThreadLocalRandom.current();
+		while (true) {
+			int year = LocalDate.now(clock).getYear() - random.nextInt(100) - 1;
+			int month = random.nextInt(13);
+			int dayOfMonth = random.nextInt(32);
+			try {
+				return LocalDate.of(year, month, dayOfMonth);
+			} catch (DateTimeException e) {
+				// ignore
+			}
+		}
+	}
+
+	private String getRandomSurname() {
+		return getRandomValue(SURNAMES);
+	}
+
+	private String getRandomForename() {
+		return getRandomValue(FORENAMES);
+	}
+
+	<T> T getRandomValue(List<T> list) {
+		double stdev = list.size() / 2;
+		int randomIndex = (int) Math.abs(ThreadLocalRandom.current().nextGaussian() * stdev);
+		int index = Math.min(randomIndex, list.size() - 1);
+		T value = list.get(index);
+		return value;
 	}
 
 }
--- src/test/java/spectum/openshiftdemo/person/PersonServiceTest.java
+++ src/test/java/spectum/openshiftdemo/person/PersonServiceTest.java
@@ -1,11 +1,21 @@
 package spectum.openshiftdemo.person;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -14,10 +24,20 @@ import reactor.test.StepVerifier;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PersonServiceTest {
+	private static final ZoneId DEFAULT_TIME_ZONE = ZoneId.systemDefault();
+	private final LocalDate referenceDate = LocalDate.of(2000, 1, 1);
+	private final Clock clock = Clock.fixed(referenceDate.atStartOfDay().atZone(DEFAULT_TIME_ZONE).toInstant(),
+			DEFAULT_TIME_ZONE);
+
 	@Mock
 	private PersonRepository personRepository;
-	@InjectMocks
+
 	private PersonService personService;
+
+	@Before
+	public void initService() {
+		personService = new PersonService(personRepository, clock);
+	}
 
 	@Test
 	public void testGetPersons() throws Exception {
@@ -28,6 +48,38 @@ public class PersonServiceTest {
 			assertEquals(testPerson, person);
 		}).expectComplete().verify();
 
+	}
+
+	@Test
+	public void testGetRandomValue() throws Exception {
+		List<String> list = Arrays.asList("A", "B", "C");
+		Set<String> hits = new HashSet<>();
+		for (int i = 0; i < 100; i++) {
+			String value = personService.getRandomValue(list);
+			assertNotNull(value);
+			hits.add(value);
+		}
+		for (String string : list) {
+			assertTrue(hits.contains(string));
+		}
+
+	}
+
+	@Test
+	public void testGetRandomBirthDate() throws Exception {
+		for (int i = 0; i < 100; i++) {
+			LocalDate birthDate = personService.getRandomBirthDate();
+			assertNotNull(birthDate);
+			assertTrue(birthDate.isBefore(referenceDate) || birthDate.isEqual(referenceDate));
+		}
+
+	}
+
+	@Test
+	public void testGetRandomCitiesOfLiving() throws Exception {
+		LocalDate birthDate = LocalDate.of(1970, 6, 1);
+		List<String> citiesOfLiving = personService.getRandomCitiesOfLiving(birthDate);
+		assertNotNull(citiesOfLiving);
 	}
 
 }
